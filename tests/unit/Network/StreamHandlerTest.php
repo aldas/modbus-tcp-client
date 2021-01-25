41dcd7fc24f11c8806955c5773351804976cdc9b
toimtoimtoim <desinformatsioon@gmail.com>
2021-01-25 21:38:47 +0200

    fix receiveFrom getting into endless loop when `stream_select` returns early but `fread` does not produce data and $lastAccess getting reset and moving timeout always into future (#73)

--- tests/unit/Network/StreamHandlerTest.php
+++ tests/unit/Network/StreamHandlerTest.php
@@ -4,6 +4,9 @@ namespace ModbusTcpClient\Network;
 
 class MockData
 {
+    // $loopForever makes mock return always same data from stream/fread. microtime returns still current time
+    public static $loopForever = false;
+
     public static $fread = null;
     public static $freadCounter = 0;
 
@@ -27,6 +30,10 @@ function fread($handle, $length)
         return \fread($handle, $length);
     }
 
+    if (MockData::$loopForever === true) {
+        return MockData::$fread[0];
+    }
+
     return MockData::$fread[MockData::$freadCounter++];
 }
 
@@ -37,6 +44,10 @@ function fread($handle, $length)
 function microtime($get_as_float = null)
 {
     if (MockData::$microtime === null) {
+        return \microtime($get_as_float);
+    }
+
+    if (MockData::$loopForever === true) {
         return \microtime($get_as_float);
     }
 
@@ -51,6 +62,10 @@ function stream_select(array &$read, array &$write, array &$except, $tv_sec, $tv
 {
     if (MockData::$stream_select === null) {
         return \stream_select($read, $write, $except, $tv_sec, $tv_usec);
+    }
+
+    if (MockData::$loopForever === true) {
+        return MockData::$stream_select[0];
     }
 
     return MockData::$stream_select[MockData::$stream_selectCounter++];
@@ -69,6 +84,7 @@ class StreamHandlerTest extends TestCase
 {
     protected function tearDown(): void
     {
+        MockData::$loopForever = false;
         MockData::$freadCounter = 0;
         MockData::$microtimeCounter = 0;
         MockData::$stream_selectCounter = 0;
@@ -88,6 +104,33 @@ class StreamHandlerTest extends TestCase
         MockData::$fread = [$returnedPacketPart1, $returnedPacketPart2];
         MockData::$microtime = [1, 1, 1, 100];
         MockData::$stream_select = ['x', 'x'];
+
+        $handler = new ForTestStreamHandler();
+
+        $readStreams = ['stream'];
+        $timeout = null;
+        $logger = new MockLogger();
+
+        $result = $handler->receiveFrom($readStreams, $timeout, $logger);
+
+        $this->assertEquals([$expected], $result);
+        $this->assertEquals(2, MockData::$freadCounter);
+
+    }
+
+    public function testSelectReturnButFreadFailure()
+    {
+        $this->expectExceptionMessage("fread error during receiveFrom");
+        $this->expectException(IOException::class);
+
+        // full packet is received with 2 fread calls (tcp packet fragmentation example)
+        $returnedPacketPart1 = "\xda\x87\x00\x00\x00\x03\x00\x81";
+        $returnedPacketPart2 = "\x03";
+        $expected = $returnedPacketPart1 . $returnedPacketPart2;
+
+        MockData::$loopForever = true;
+        MockData::$fread = [false]; // fread() returns false - something is wrong with stream/socket
+        MockData::$stream_select = ['x'];
 
         $handler = new ForTestStreamHandler();
 
