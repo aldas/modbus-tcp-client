9cb1f4a176c5652d473e5eeec61e0b968a718358
toimtoimtoim <desinformatsioon@gmail.com>
2021-05-09 18:50:23 +0300

    Allow high level api to specify custom endian for string address

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [2.3.0] - 2021-05-09
+
+### Added
+
+* Allow high level API setting custom endian for `string` address with `ReadRegistersBuilder` and `StringReadRegisterAddress` classes.
+
 ## [2.2.0] - 2021-05-04
 
 ### Added
--- tests/unit/Composer/Read/Register/StringReadRegisterAddressTest.php
+++ tests/unit/Composer/Read/Register/StringReadRegisterAddressTest.php
@@ -5,6 +5,7 @@ namespace Tests\unit\Composer\Read\Register;
 
 use ModbusTcpClient\Composer\Read\Register\StringReadRegisterAddress;
 use ModbusTcpClient\Packet\ModbusFunction\ReadHoldingRegistersResponse;
+use ModbusTcpClient\Utils\Endian;
 use PHPUnit\Framework\TestCase;
 
 class StringReadRegisterAddressTest extends TestCase
@@ -39,6 +40,7 @@ class StringReadRegisterAddressTest extends TestCase
 
     public function testExtractWithCallback()
     {
+        //      big endian + low word first                                     | ø | S | e | r | \0| b
         $responsePacket = new ReadHoldingRegistersResponse("\x08\x01\x00\xF8\x53\x65\x72\x00\x6E", 3, 33152);
         $address = new StringReadRegisterAddress(1, 5, 'username', function ($value) {
             return 'prefix_' . $value; // transform value after extraction
@@ -47,6 +49,17 @@ class StringReadRegisterAddressTest extends TestCase
         $value = $address->extract($responsePacket);
 
         $this->assertEquals('prefix_Søren', $value);
+    }
+
+    public function testExtractWithEndian()
+    {
+        //      little endian                                                   | S | ø | r | e | b | \0
+        $responsePacket = new ReadHoldingRegistersResponse("\x08\x00\x01\x53\xF8\x72\x65\x6E\x00", 3, 33152);
+        $address = new StringReadRegisterAddress(1, 5, 'username', null, null, Endian::LITTLE_ENDIAN);
+
+        $value = $address->extract($responsePacket);
+
+        $this->assertEquals('Søren', $value);
     }
 
 }
