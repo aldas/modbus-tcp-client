514be3844a2fbda3866fb31251063c77b4543e13
toimtoimtoim <desinformatsioon@gmail.com>
2022-10-16 21:56:17 +0300

    Added `Packet::isCompleteLengthRTU()` to help checking if packet is complete RTU packet

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+# [3.1.0] - 2022-10-16
+
+* Added `Packet::isCompleteLengthRTU()` to help checking if packet is complete RTU packet. Helps when receiving fragmented packets.
+* Example for RTU over TCP with higher level API [examples/rtu_over_tcp_with_higherlevel_api.php](examples/rtu_over_tcp_with_higherlevel_api.php)
+
 ## [3.0.1] - 2022-09-29
 
 * `ResultContainer.offsetGet` was missing return type
--- src/Utils/Packet.php
+++ src/Utils/Packet.php
@@ -5,6 +5,7 @@ namespace ModbusTcpClient\Utils;
 
 
 use ModbusTcpClient\Network\IOException;
+use ModbusTcpClient\Packet\ErrorResponse;
 
 final class Packet
 {
@@ -14,11 +15,11 @@ final class Packet
     }
 
     /**
-     * isCompleteLength checks if binary string is complete modbus packet
+     * isCompleteLength checks if binary string is complete modbus TCP packet
      * NB: this function works only for MODBUS TCP packets
      *
      * @param string|null $binaryData binary string to be checked
-     * @return bool true if data is actual error packet
+     * @return bool true if data is actual modbus TCP packet
      */
     public static function isCompleteLength(string|null $binaryData): bool
     {
@@ -30,6 +31,37 @@ final class Packet
         // modbus header 6 bytes are = transaction id + protocol id + length of PDU part.
         // so adding these number is what complete packet would be
         $expectedLength = 6 + unpack('n', ($binaryData[4] . $binaryData[5]))[1];
+
+        if ($length > $expectedLength) {
+            throw new IOException('packet length more bytes than expected');
+        }
+        return $length === $expectedLength;
+    }
+
+    /**
+     * isCompleteLengthRTU checks if binary string is complete modbus RTU packet
+     * NB: this function works only for MODBUS RTU packets
+     *
+     * @param string|null $binaryData binary string to be checked
+     * @return bool true if data is actual error packet
+     */
+    public static function isCompleteLengthRTU(string|null $binaryData): bool
+    {
+        // minimal RTU packet length is 5 bytes (1 byte unit id + 1 byte function code + 1 byte of error code or byte length + 2 bytes for CRC)
+        $length = strlen($binaryData);
+        if ($length < 5) {
+            return false;
+        }
+        if ((ord($binaryData[1]) & ErrorResponse::EXCEPTION_BITMASK) > 0) { // seems to be error response
+            return true;
+        }
+        // if it is not error response then 3rd byte contains data length in bytes
+
+        // trailing 3 bytes are = unit id + function code + data length in bytes
+        // next is N bytes of data that should match 3rd byte value
+        // and 2 bytes for CRC
+        // so adding these number is what complete packet would be
+        $expectedLength = 3 + ord($binaryData[2]) + 2;
 
         if ($length > $expectedLength) {
             throw new IOException('packet length more bytes than expected');
