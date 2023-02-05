dbe3828f76c133624bc7b42ec6c80dbfe481573b
Uldis Nelsons <uldis@weberp.lv>
2023-02-05 07:55:34 +0200

    fixed

--- tests/unit/Composer/Read/Register/ReadByteAddressSplitterTest.php
+++ tests/unit/Composer/Read/Register/ReadByteAddressSplitterTest.php
@@ -3,7 +3,6 @@
 namespace Tests\unit\Composer\Read\Register;
 
 use ModbusTcpClient\Composer\AddressSplitter;
-use ModbusTcpClient\Composer\Read\Register\ByteReadRegisterAddress;
 use ModbusTcpClient\Composer\Read\Register\ReadRegisterAddressSplitter;
 use PHPUnit\Framework\TestCase;
 
@@ -15,8 +14,8 @@ class ReadByteAddressSplitterTest extends TestCase
         $splitter = new ReadRegisterAddressSplitter(ByteReadRegisterAddress::class);
         $requests = $splitter->split([
             'tcp://127.0.0.1' . AddressSplitter::UNIT_ID_PREFIX . '1' => [
-                new ByteReadRegisterAddress(1,false),
-                new ByteReadRegisterAddress(1, true),
+                new ModbusTcpClient\Composer\Read\Register\ByteReadRegisterAddress(1,false),
+                new ModbusTcpClient\Composer\Read\Register\ByteReadRegisterAddress(1, true),
             ]
         ]);
         $this->assertCount(1, $requests);
